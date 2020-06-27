35c11468197d31cf1377c6bcbf6b26045ac033a0
AndresRicardoSarmiento <66749337+AndresRicardoSarmiento@users.noreply.github.com>
2020-06-27 11:46:15 -0500

    Prueba2

--- app/src/main/java/com/example/caculadora/MainActivity.java
+++ app/src/main/java/com/example/caculadora/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btn_suma;
     private Button btn_resta;
-    
+
 
     private Button btn_division;
     private Button btn_multiplicacion;
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView text_respuesta;
     private EditText edit_num1;
     private EditText edit_num2;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
